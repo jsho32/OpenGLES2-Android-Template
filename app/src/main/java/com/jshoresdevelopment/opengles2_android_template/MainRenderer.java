573f107c790040b795a448fdb6b7b2e3cb78c1c3
jsho32 <justin.shores32@gmail.com>
2015-07-01 00:05:50 -0700

    Add gl surfaceview and renderer.

---
+++ app/src/main/java/com/jshoresdevelopment/opengles2_android_template/MainRenderer.java
@@ -0,0 +1,26 @@
+package com.jshoresdevelopment.opengles2_android_template;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by justinshores on 7/1/15.
+ */
+public class MainRenderer implements Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
---
+++ app/src/main/java/com/jshoresdevelopment/opengles2_android_template/MainSurfaceView.java
@@ -0,0 +1,14 @@
+package com.jshoresdevelopment.opengles2_android_template;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by justinshores on 7/1/15.
+ */
+public class MainSurfaceView extends GLSurfaceView {
+
+    public MainSurfaceView(Context context) {
+        super(context);
+    }
+}
